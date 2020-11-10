bde5e497a6ae7c90d9d1c8d6c476c275ed745817
Nuno Afonso Araújo Pereira <nunoaapp22@hotmail.com>
2020-11-10 22:17:08 +0000

    CBC

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 import base64
 import os
-def GetBytesImage():
+def ECB():
 
-    key = b'1234123412341234'
+    #key = b'1234123412341234'
+    key=os.urandom(32)
     print(key.__sizeof__())
     iv = os.urandom(16)
     cipher = Cipher(algorithms.AES(key), modes.ECB())
@@ -13,12 +14,8 @@ def GetBytesImage():
     with open("5.bmp", "rb") as imageFile:
         datas = base64.b64encode(imageFile.read())
         imageFile.close()
-
-    ct = encryptor.update(datas[:48]) + encryptor.finalize()
-    c = datas[48:] + datas[:datas.__len__()]
+    ct = encryptor.update(datas+datas[:12]) + encryptor.finalize()
     newdatas = ct.hex().encode('utf8')
-    newdatas = newdatas.__add__(c)
-
     newdatas.decode('utf8')
     m = base64.b64decode(newdatas)
     fh = open("3.bmp", "wb")
@@ -26,7 +23,26 @@ def GetBytesImage():
     fh.close()
     os.system("dd if=5.bmp of=3.bmp bs=1 count=54 conv=notrunc")
 
+def CBC():
 
-# Press the green button in the gutter to run the script.
+    key = os.urandom(32)
+    print(key.__sizeof__())
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+    encryptor = cipher.encryptor()
+    with open("5.bmp", "rb") as imageFile:
+        datas = base64.b64encode(imageFile.read())
+        imageFile.close()
+
+    ct = encryptor.update(datas.__add__(datas[:12])) + encryptor.finalize()
+    newdatas = ct.hex().encode('utf8')
+    newdatas.decode('utf8')
+    m = base64.b64decode(newdatas)
+    fh = open("6.bmp", "wb")
+    fh.write(m)
+    fh.close()
+    os.system("dd if=5.bmp of=6.bmp bs=1 count=54 conv=notrunc")
+
 if __name__ == '__main__':
-    GetBytesImage()
+    ECB()
+    CBC()
